c9a51c2314068ebfe5971770a0f2522abaa93fff
Wangmo Tenzing <wtenzing6@gmail.com>
2023-06-16 23:23:14 -0700

    first assignment

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+
 def main():
-
-    ##############################
-    # make your code below
-    # print('Hello World')
-    ##############################
-
+    print('Hello World')
+    print('Python Programming')
 
 if __name__ == '__main__':
     main()
+
+
